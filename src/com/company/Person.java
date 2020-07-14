2c9a980177d57644094fb544bd8ee9b31366cae1
eftychia.spyropoulou <eftychia_sp@yahoo.gr>
2020-07-14 12:19:05 +0300

    New class: Person

---
+++ src/com/company/Person.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Person {
+    private String Firstname;
+
+    public void setFirstname (String firstname){
+        Firstname=firstname;
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+}
